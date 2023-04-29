145a57456c4584cbb79e7587d6d09ba90fde745f
TantoFaz <thiago.playgames15@gmail.com>
2023-04-29 10:46:21 -0300

    doc update

--- src/client/commands/tests/ping.ts
+++
@@ -1,9 +0,0 @@
-import ICommand from "../../interfaces/command"
-
-export const command: ICommand = {
-    name: "ping",
-    description: "Envia o ping do bot",
-    run: async (context) => {
-        await context.replyText("Pong!")
-    }
-}
